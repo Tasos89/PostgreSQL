5b31588269e1cfffc43e6d025da67ddf3b6da6b6
Vogiatzis <a.vogiatzis@fugro.com>
2021-03-17 16:49:03 +0100

    second commit

--- first_SQL.py
+++ first_SQL.py
@@ -1,14 +1,20 @@
 import psycopg2
+import psycopg2.extras
 
 con = psycopg2.connect(database='testDB', user='postgres', password='skatastamoutra', host='localhost')
 
-cur = con.cursor()
+cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-# cur.execute('ISERT INTO person (id Name Email Age);')
+# cur.execute('INSERT INTO person (id Name Email Age) VALUES(%s)', (5,'Malakas','tooltasos#hotmail.com',33,))
+# cur.execute('INSERT INTO person (Name)', ('Malakas',))
 
-# cur.execute('INSERT INTO (Name) VALUES(%s)', ('Malakas'))
 
-cur.execute("SELECT * FROM person;")
+cur.execute("SELECT * FROM person WHERE id = %s;", (3,))
 
-print(cur.fetchall())
+print('The person in table who has id = 2 is :', cur.fetchone()['Name'])
 
+con.commit()
+
+cur.close()
+
+con.close()
